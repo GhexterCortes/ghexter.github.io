b0c2356faf7d41822a5efad5af20d8469308c8fc
GhexterCortes <ghextershumies@gmail.com>
2022-02-05 04:03:58 +0800

    addEmotions to texts

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 const defaultStyle = '/styles/default.css';
 const titles = ["<3", ":)", ":D", ":P", ":O", ":3", ":*", ":^)", ":v", ":>", ":<", ":|", ":S", ":X", ":B"];
 
-function setTitle(element) {
-    element.innerHTML = `Ghexter Cortes ${titles[Math.floor(Math.random() * titles.length)]}`;
+function addEmotions(element, text) {
+    text = text || element.innerHTML;
+
+    element.innerHTML = `${text} ${titles[Math.floor(Math.random() * titles.length)]}`;
 
     setTimeout(() => {
-        setTitle(element);
+        addEmotions(element, text);
     }, 4000);
 }
