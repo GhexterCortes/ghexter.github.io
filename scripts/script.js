9ac5e77e9988e2fbd107a9c8889b79d702fd2f74
GhexterCortes <ghextershumies@gmail.com>
2022-02-05 13:56:11 +0800

    added about me

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 const defaultStyle = '/styles/default.css';
 const titles = ["<3", ":)", ":D", ":P", ":O", ":3", ":*", ":^)", ":v", ":>", ":<", ":|", ":S", ":X", ":B"];
 
-function addEmotions(element, text) {
+function addEmotions(element, text, emotes = titles) {
     text = text || element.innerHTML;
+    text = typeof text === 'string' ? [text] : text;
 
-    element.innerHTML = `${text} ${titles[Math.floor(Math.random() * titles.length)]}`;
+    element.innerHTML = `${text[Math.floor(Math.random() * text.length)]} ${emotes[Math.floor(Math.random() * emotes.length)]}`;
 
     setTimeout(() => {
-        addEmotions(element, text);
+        addEmotions(element, text, emotes);
     }, 4000);
 }
