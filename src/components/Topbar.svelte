aae02f6fd677000deb5b5603753fc2819a5416e3
Ghex <ghextershumies@gmail.com>
2022-09-30 01:34:11 +0800

    first page wtf

---
+++ src/components/Topbar.svelte
@@ -0,0 +1,30 @@
+<script>
+    import '../assets/css/topbar.scss';
+    import Icon from "@iconify/svelte";
+    import { github } from "../scripts/info";
+
+    let navOpen = false;
+</script>
+
+<div class="topbar" class:active={navOpen}>
+    <div class="container">
+        <div class="title">
+            <a href="/" on:click={e => { e.preventDefault(); window.scrollTo(0, 0); }}>Not Ghex</a>
+            <div class="burger">
+                <button class="cheese" on:click={e => navOpen = !navOpen}>
+                    {#if navOpen}
+                    <Icon icon="ph:x" inline={true} />
+                    {:else}
+                    <Icon icon="ph:equals-bold" inline={true} />
+                    {/if}
+                </button>
+            </div>
+        </div>
+        <div class="links">
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+            <a href="{github}">GitHub <Icon icon="ci:external-link" inline={true} /></a>
+        </div>
+    </div>
+</div>
---
+++ src/sections/Home.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    import { github } from '../scripts/info';
+    import Topbar from '../components/Topbar.svelte';
+</script>
+
+<style lang="scss">
+    @use '../assets/css/e.scss';
+
+    .home {
+        height: 100%;
+        min-height: 500px;
+        display: block;
+        position: relative;
+        background: e.$theme;
+        color: e.$light;
+
+        .container {
+            position: relative;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .contents {
+                width: 100%;
+                padding: 5rem 2rem;
+                max-width: 900px;
+                letter-spacing: 2px;
+                font-family: 'Abyssinica SIL';
+
+                .greetings {
+                    font-size: 1.1rem;
+                    color: rgba($color: e.$secondary, $alpha: 0.9);
+                    font-weight: 700;
+                }
+
+                .title {
+                    font-size: 5rem;
+                    font-weight: 700;
+                    line-height: 5rem;
+
+                    .highlight {
+                        transition: 0.3s;
+
+                        &:hover {
+                            color: e.$primary;
+                        }
+                    }
+                }
+
+                .intro {
+                    margin-top: 0.5rem;
+                    font-family: 'Open Sans';
+                    color: rgba($color: e.$light, $alpha: 0.8);
+                    font-size: 0.97rem;
+                    max-width: 55%;
+                }
+
+                .buttons {
+                    display: flex;
+                    margin-top: 2rem;
+
+                    a {
+                        padding: 0.5rem 1rem;
+                        color: e.$theme;
+                        border: 2px solid e.$primary;
+                        background: e.$primary;
+                        text-decoration: none;
+                        font-weight: 700;
+                        margin-right: 1rem;
+
+                        &:hover,
+                        &:focus,
+                        &:last-child {
+                            background: e.$theme;
+                            color: e.$primary;
+                        }
+
+                        &:last-child {
+                            &:hover,
+                            &:focus {
+                                background: e.$primary;
+                                color: e.$theme;
+                            }
+                        }
+
+                        &:focus-visible {
+                            border-color: e.$secondary;
+                            outline: none;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
+
+<div class="home">
+    <Topbar/>
+    <div class="container">
+        <div class="contents">
+            <span class="greetings">
+                Hi, There
+            </span>
+            <h1 class="title">
+                I am <span class="highlight">Ghex</span>
+            </h1>
+            <p class="intro">I do simple high level software thingz like any other developer.</p>
+            <div class="buttons">
+                <a href="#about">About</a>
+                <a href="{github}">View GitHub <Icon icon="bi:arrow-right-short" inline={true} /></a>
+            </div>
+        </div>
+    </div>
+</div>
