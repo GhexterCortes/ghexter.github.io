36678f750d6d8864b6e6071f76e085bcc1b8dc66
Ghex <ghextershumies@gmail.com>
2022-09-30 21:39:01 +0800

    some settings

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Footer from './components/Footer.svelte';
     import Topbar from './components/Topbar.svelte';
+    import { avatar, description } from './scripts/info';
     import About from './sections/About.svelte';
     import Home from './sections/Home.svelte';
     import Projects from './sections/Projects.svelte';
@@ -14,3 +15,12 @@
     <Projects/>
     <Footer/>
 </div>
+
+
+<svelte:head>
+    <link rel="shortcut icon" href="{avatar}" type="image/x-icon">
+    <meta name="description" content="{description}">
+    <meta property="og:title" content="Not Ghex" />
+    <meta property="og:description" content="{description}">
+	<meta property="og:image" content="{avatar}">
+</svelte:head>
